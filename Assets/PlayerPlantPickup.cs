bf4537205fa4df7ef9009ee1e40c02242d996b70
night-aliceio <aliceionash@gmail.com>
2025-05-06 13:07:09 -0600

    added sound

--- Assets/PlayerPlantPickup.cs
+++ Assets/PlayerPlantPickup.cs
@@ -7,13 +7,23 @@ using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
+
+  
     [Header("Inventory UI")]
     public Transform inventoryPanel;
     public GameObject inventorySlotPrefab;
 
-    private List<Sprite> collectedItemIcons = new List<Sprite>();
+    [Header("Audio")]
+    public AudioClip collectSound;
+    private AudioSource audioSource;
 
+    private List<Sprite> collectedItemIcons = new List<Sprite>();
     private Dictionary<string, List<Sprite>> inventory = new Dictionary<string, List<Sprite>>();
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void PlantCollected(Sprite itemIcon)
     {
@@ -28,15 +38,19 @@ public class PlayerInventory : MonoBehaviour
             inventory.Add(itemName, new List<Sprite>());
         }
 
-        // Add the icon to the list of sprites under the itemName
         if (!inventory[itemName].Contains(icon))
         {
             inventory[itemName].Add(icon);
         }
-        AddItemToUI(icon);
-    }
 
-    //method to remove item
+        AddItemToUI(icon);
+
+        // Play collection sound
+        if (collectSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(collectSound);
+        }
+    }
 
     public bool HasItem(Sprite spriteToCheck)
     {
@@ -44,7 +58,6 @@ public class PlayerInventory : MonoBehaviour
         {
             foreach (var icon in entry.Value)
             {
-                // Check if any of the sprites match the given sprite
                 if (icon.name == spriteToCheck.name)
                 {
                     return true;
